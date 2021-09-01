2f4be8b33d0cec3f64c7ee1ebeb06316e9770f53
Manjeet <54723481+Manjeets666@users.noreply.github.com>
2021-09-01 23:22:58 +0530

    Create ERC20_contract.sol

---
+++ ERC20_contract.sol
@@ -0,0 +1,93 @@
+pragma solidity ^0.5.0;
+// ERC Token Standard #20 Interface
+
+contract ERC20Interface {
+    function totalSupply() public view returns (uint);
+    function balanceOf(address tokenOwner) public view returns (uint balance);
+    function allowance(address tokenOwner, address spender) public view returns (uint remaining);
+    function transfer(address to, uint tokens) public returns (bool success);
+    function approve(address spender, uint tokens) public returns (bool success);
+    function transferFrom(address from, address to, uint tokens) public returns (bool success);
+    function mint(address tokenOwner, uint mintedTokens) public;
+    function burn(address account, uint tokens) public returns (bool success);
+
+    event Transfer(address indexed from, address indexed to, uint tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+}
+
+contract Token is ERC20Interface {
+    string public name;
+    address public owner; 
+    string public symbol;
+    uint8 public decimals; // by default 18
+
+    uint256 public _totalSupply;
+
+    mapping(address => uint) balances;
+    mapping(address => mapping(address => uint)) allowed;
+
+    // Initializes contract with initial supply tokens to the creator of the contract.
+    
+    constructor() public {
+        owner=msg.sender;
+        name = "Manjeet";
+        symbol = "MS";
+        decimals = 18;
+        _totalSupply = 100000000000000000000000000;
+
+        balances[msg.sender] = _totalSupply;
+        emit Transfer(address(0), msg.sender, _totalSupply);
+    }
+
+    function totalSupply() public view returns (uint) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address tokenOwner) public view returns (uint ) {
+        return balances[tokenOwner];
+    }
+
+    function allowance(address tokenOwner, address spender) public view returns (uint remaining) {
+        return allowed[tokenOwner][spender];
+    }
+
+    function approve(address spender, uint tokens) public returns (bool success) {
+        allowed[msg.sender][spender] = tokens;
+        emit Approval(msg.sender, spender, tokens);
+        return true;
+    }
+
+    function transfer(address to, uint tokens) public returns (bool success) {
+        require(balances[msg.sender]>=tokens);
+        balances[msg.sender]-= tokens;
+        balances[to]+= tokens;
+        emit Transfer(msg.sender, to, tokens);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint tokens) public returns (bool success) {
+        require(tokens<= balances[from]);
+        require(tokens<=allowed[from][msg.sender]); 
+        balances[from]-= tokens;
+        allowed[from][msg.sender]-= tokens;
+        balances[to]+= tokens;
+        emit Transfer(from, to, tokens);
+        return true;
+    }
+    function mint(address tokenOwner, uint mintedTokens) public {
+        require(msg.sender==owner);
+        balances[tokenOwner]+=mintedTokens;
+        _totalSupply+=mintedTokens;
+        emit Transfer(address(0),tokenOwner,mintedTokens);
+    } 
+    function burn(address account, uint tokens) public returns (bool success){
+        require(account != address(0), "ERC20: burn from the zero address");
+        require(balances[msg.sender]>=tokens);
+        balances[account]-=tokens;
+        _totalSupply-=tokens;
+        emit Transfer(account,address(0),tokens);
+        return true;
+        
+    }
+    
+}
